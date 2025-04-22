352dba886373dc822e0f6926c51d14bf317aa952
Ренькас Альберт Олегович <ipz235_rao@student.ztu.edu.ua>
2025-04-22 20:27:15 +0300

    lab-4_task4

---
+++ task5/NetworkImageLoad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    public class NetworkImageLoad:IImageLoad
+    {
+        public byte[] Load(string path)
+        {
+            Console.WriteLine($"Downloading from URL: {path}");
+            using var client = new HttpClient();
+            return client.GetByteArrayAsync(path).Result;
+        }
+    }
+}
---
+++ task5/Program.cs
@@ -0,0 +1,29 @@
+﻿using task5;
+
+internal class Program
+{
+    private static void Main()
+    {
+        LightElementNode div = new LightElementNode("div");
+        div.AddClass("container");
+
+        LightElementNode ul = new LightElementNode("ul");
+        ul.AddClass("list");
+
+        ul.AddChild(new LightElementNode("li") { Children = { new LightTextNode("Item 1") } });
+        ul.AddChild(new LightElementNode("li") { Children = { new LightTextNode("Item 2") } });
+        ul.AddChild(new LightElementNode("li") { Children = { new LightTextNode("Item 3") } });
+
+        div.AddChild(ul);
+
+        Console.WriteLine(div.OuterHTML);
+        var imgLocal = new LightImageNode(@"C:\Users\posta\OneDrive\Изображения\SplitCam\8Ksj.gif");
+        var imgNet = new LightImageNode("https://media.tenor.com/x8v1oNUOmg4AAAAM/rickroll-roll.gif");
+        div.AddChild(ul);
+        div.AddChild(imgLocal);
+        div.AddChild(imgNet);
+        File.WriteAllText("output.html", div.OuterHTML);
+       
+        Console.WriteLine("Завантаження зображень завершено.");
+    }
+}
