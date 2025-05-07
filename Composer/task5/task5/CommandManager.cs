c9d200f761884f4dd11d1706233b460f13ff8ab9
Ренькас Альберт Олегович <ipz235_rao@student.ztu.edu.ua>
2025-05-07 21:03:42 +0300

    Command

---
+++ Composer/task5/task5/CommandManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    public class CommandManager
+    {
+        private readonly Stack<ICommand> undoStack = new();
+        private readonly Stack<ICommand> redoStack = new();
+
+        public void Execute(ICommand cmd)
+        {
+            cmd.Execute();
+            undoStack.Push(cmd);
+            redoStack.Clear();
+        }
+
+        public void Undo()
+        {
+            if (!undoStack.Any()) return;
+            var cmd = undoStack.Pop();
+            cmd.Undo();
+            redoStack.Push(cmd);
+        }
+
+        public void Redo()
+        {
+            if (!redoStack.Any()) return;
+            var cmd = redoStack.Pop();
+            cmd.Execute();
+            undoStack.Push(cmd);
+        }
+    }
+}
--- Composer/task5/task5/Program.cs
+++ Composer/task5/task5/Program.cs
@@ -4,18 +4,23 @@ internal class Program
 {
     private static void Main()
     {
-        LightElementNode div = new LightElementNode("div");
-        div.AddClass("container");
-
-        LightElementNode ul = new LightElementNode("ul");
+        var appRoot = new LightElementNode("div");
+        appRoot.AddClass("container");
+        var ul = new LightElementNode("ul");
         ul.AddClass("list");
-
         ul.AddChild(new LightElementNode("li") { Children = { new LightTextNode("Item 1") } });
         ul.AddChild(new LightElementNode("li") { Children = { new LightTextNode("Item 2") } });
         ul.AddChild(new LightElementNode("li") { Children = { new LightTextNode("Item 3") } });
-
-        div.AddChild(ul);
-
-        Console.WriteLine(div.OuterHTML);
+        appRoot.AddChild(ul);
+        Console.WriteLine(appRoot.OuterHTML);
+        Console.WriteLine("Macro Command");
+        var cmdMgr = new CommandManager();
+        var macro = new MacroCommand();
+        macro.Add(new AddElementCommand(appRoot, "button"));
+        macro.Add(new AddClassCommand(appRoot, "btn-primary"));
+        cmdMgr.Execute(macro);
+        Console.WriteLine(appRoot.OuterHTML);
+        cmdMgr.Undo();
+        Console.WriteLine("After undo: " + appRoot.OuterHTML);
     }
 }
